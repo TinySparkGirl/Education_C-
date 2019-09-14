56a092afeafeac6b68e7c77106d47161f2093efa
Tatyana <hr-managment@bk.ru>
2019-09-14 20:19:39 +0300

    начало работы в C#, первое задание

---
+++ lesson_1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace lesson_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Я хочу изучать C#!");
+            Console.ReadLine();
+        }
+    }
+}
