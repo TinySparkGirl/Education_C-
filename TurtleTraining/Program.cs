c8ade24edeb10c48e7fbbf820ae5f20c763655d8
Tatyana <hr-managment@bk.ru>
2019-09-20 19:00:38 +0300

    Урок по созданию черепашки. Задание - черепашка ходит спиралью и шестигранник

---
+++ TurtleTraining/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SmallBasic.Library;
+
+namespace TurtleTraining
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Turtle.Speed = 7;
+                int i = 0;
+                while (i < 3)
+                {
+                    int h = 0;
+                    while (h < 2)
+                    {
+                        Turtle.Move(100);
+                        Turtle.TurnRight();
+                        h++;
+                    }
+                    int g = 0;
+                    while (g < 2)
+                    {
+                        Turtle.Move(100);
+                        Turtle.TurnLeft();
+                        g++;
+                    }
+                    i++;
+                }
+
+                Turtle.Speed = 7;
+                int k = 0;
+                while (k < 8)
+                {
+                    Turtle.Move(100);
+                    Turtle.Turn(45);
+                    k++;
+                }
+
+            }
+        }
+    }
+}
