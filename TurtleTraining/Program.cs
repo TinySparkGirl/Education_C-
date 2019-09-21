016facdc4721b02fc820c8096179221b2179d2ad
Tatyana <hr-managment@bk.ru>
2019-09-21 14:36:00 +0300

    Задание. Пишем слово торт. Буква Т с большой буквы. Пишем имя близкого человека.

--- TurtleTraining/Program.cs
+++ TurtleTraining/Program.cs
@@ -9,40 +9,114 @@ namespace TurtleTraining
 {
     class Program
     {
-        static void Main(string[] args)
+        //static void WriteT(int size)
+        //{
+        //    Turtle.Angle = 0;
+        //    Turtle.Move(size);
+        //    Turtle.Angle = 270;
+        //    Turtle.Move(20);
+        //    Turtle.Angle = 90;
+        //    Turtle.Move(40);
+        //}
+
+        static void WriteB()
         {
-            {
-                Turtle.Speed = 7;
-                int i = 0;
-                while (i < 3)
-                {
-                    int h = 0;
-                    while (h < 2)
-                    {
-                        Turtle.Move(100);
-                        Turtle.TurnRight();
-                        h++;
-                    }
-                    int g = 0;
-                    while (g < 2)
-                    {
-                        Turtle.Move(100);
-                        Turtle.TurnLeft();
-                        g++;
-                    }
-                    i++;
-                }
-
-                Turtle.Speed = 7;
-                int k = 0;
-                while (k < 8)
-                {
-                    Turtle.Move(100);
-                    Turtle.Turn(45);
-                    k++;
-                }
-
-            }
+            Turtle.Angle = 0;
+            Turtle.Move(100);
+            Turtle.Angle = 90;
+            Turtle.Move(40);
+            Turtle.Angle = 180;
+            Turtle.Move(30);
+            Turtle.Angle = 270;
+            Turtle.Move(40);
+            Turtle.Angle = 90;
+            Turtle.Move(50);
+            Turtle.Angle = 180;
+            Turtle.Move(70);
+            Turtle.Angle = 270;
+            Turtle.Move(50);
         }
+
+
+        static void Main(string[] args)
+        //{
+        //    {
+        //        // пишем слово ТОРТ
+
+        //        Turtle.Speed = 9;
+        //        Turtle.X = 200;
+        //        Turtle.Y = 200;
+
+        //        WriteT(100);
+
+        //        Turtle.X = 260;
+        //        Turtle.Y = 200;
+
+        //        Turtle.Angle = 0;
+        //        for (int i = 0; i < 4; i++ )
+        //        {
+        //            Turtle.Move(60);
+        //            Turtle.TurnRight();                    
+        //        }
+
+        //        Turtle.X = 380;
+        //        Turtle.Y = 200;
+
+        //        Turtle.Angle = 0;
+        //        Turtle.Move(60);
+        //        for (int j = 0; j < 3; j++ )
+        //        {
+        //            Turtle.TurnRight();
+        //            Turtle.Move(30);                    
+        //        }
+
+        //        Turtle.X = 480;
+        //        Turtle.Y = 200;
+
+        //        WriteT(60);
+        //    }
+        //}
+        {
+            //пишем имя Вова
+            Turtle.Speed = 9;
+            Turtle.X = 200;
+            Turtle.Y = 200;
+
+            WriteB();
+
+            Turtle.X = 280;
+            Turtle.Y = 200;
+
+            Turtle.Angle = 0;
+            for (int i = 0; i < 2; i++)
+            {
+                Turtle.Move(100);
+                Turtle.TurnRight();
+                Turtle.Move(50);
+                Turtle.TurnRight();                
+            }
+
+            Turtle.X = 360;
+            Turtle.Y = 200;
+            WriteB();
+
+            Turtle.X = 440;
+            Turtle.Y = 200;
+
+            Turtle.Angle = 0;
+            Turtle.Move(100);
+            Turtle.Angle = 90;
+            Turtle.Move(50);
+            Turtle.Angle = 180;
+            Turtle.Move(30);
+            Turtle.Angle = 270;
+            Turtle.Move(50);
+            Turtle.Angle = 90;
+            Turtle.Move(50);
+            Turtle.Angle = 180;
+            Turtle.Move(70);
+        }
+
+
     }
 }
